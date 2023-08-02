6e1fad8b2cb77cfdb9c328a3ca6ebf31f85bcc73
Giulio <giuliocollesei@gmail.com>
2023-08-02 11:37:59 +0200

    documentation

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,27 @@ function App() {
   return (
     <>
       <Leva>
+        <div style={{ textAlign: "left" }}>
+          <h2>Change the parameters in the dialog →</h2>
+          <pre>
+            <code>
+              minRadius: controls how small a circle can be
+              <br />
+              maxRadius: controls how big a circle can be
+              <br />
+              totalCircles: the numbers of circles
+              <br />
+              intersection: percentage (0 to 1) of how much a circle can
+              intersect another circle. 1 is equivalent to the radius of the
+              second circle
+              <br />
+              colorRange: primary color will be randomly lightned or darkned
+              within this range
+            </code>
+          </pre>
+
+          <h3>These are 20 canvas generated with the current parameters:</h3>
+        </div>
         <div
           style={{
             display: "flex",
